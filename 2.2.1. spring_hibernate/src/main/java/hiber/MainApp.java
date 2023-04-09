78c9b7dc7f23bd26ce43dbdebd472dac216cc759
kakamkina <karinakamkina@gmail.com>
2023-04-09 20:35:44 +0300

    upd

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,7 +1,9 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
+import hiber.service.CarService;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,26 +11,66 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
 
-      UserService userService = context.getBean(UserService.class);
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        UserService userService = context.getBean(UserService.class);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+        userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
-      context.close();
-   }
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println();
+        }
+
+        CarService carService = context.getBean(CarService.class);
+
+        User user01 = new User("Vasya", "Pupkin", "kk@me.com");
+        userService.add(user01);
+        Car car01 = new Car(user01, "priora", 666);
+        carService.add(car01);
+        user01.setCar(car01);
+        car01.setUser(user01);
+
+        User user02 = new User("Rybku", "S'est", "iKodNapisat'@me.com");
+        userService.add(user02);
+        Car car02 = new Car(user02, "bicycle", 1001);
+        carService.add(car02);
+        user02.setCar(car02);
+        car02.setUser(user02);
+
+        User user03 = new User("Nikola", "Tesla", "tesla1856@me.com");
+        userService.add(user03);
+        Car car03 = new Car(user03, "s", 2021);
+        carService.add(car03);
+        user03.setCar(car03);
+        car03.setUser(user03);
+
+        List<Car> cars = carService.listCars();
+        for (Car car : cars) {
+            System.out.println("Model = " + car.getModel());
+            System.out.println("Series = " + car.getSeries());
+            System.out.println();
+        }
+
+        List<User> getUserByCar = userService.getUserByCar("priora", 666);
+        for (User user : getUserByCar) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println();
+        }
+
+        context.close();
+    }
 }
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,62 @@
+package hiber.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Car")
+public class Car implements Serializable {
+
+    @Id
+    @OneToOne()
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(User user, String model, int series) {
+        this.user = user;
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "user=" + user.getFirstName() +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
